036a0122bff12c7722fb706d4a1bab1f8feb5104
Miguel Ruiz <later481@gmail.com>
2022-03-21 20:26:55 -0600

    cambios js

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ const fetchPokemon = () => {
             document.getElementById('type').innerHTML = data.types[0].type.name.toUpperCase();
             document.getElementById('move').innerHTML = data.moves[0].move.name.toUpperCase();
             document.getElementById('move1').innerHTML = data.moves[1].move.name.toUpperCase();
+            document.getElementById('hp').innerHTML = data.stats[0].base_stat;
+            document.getElementById('atk').innerHTML = data.stats[1].base_stat;
+            document.getElementById('dfs').innerHTML = data.stats[2].base_stat;
             console.log(data);
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
